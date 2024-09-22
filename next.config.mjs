ed7eea60b10ee887f0e75a74d5a11b6073f34a19
soummyaanon <soumyaranjanpanda910@gmail.com>
2024-09-22 13:23:09 +0530

    refactor: Improve error handling in middleware and add custom headers for security

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,38 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    webpack: (config) => {
+      config.experiments = { ...config.experiments, topLevelAwait: true };
+      return config;
+    },
+    // Increase the serverless function timeout to 60 seconds (maximum allowed by Vercel)
+    serverRuntimeConfig: {
+      functionTimeout: 60,
+    },
+    // Add custom headers for security
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
